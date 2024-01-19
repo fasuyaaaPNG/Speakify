06df21a7d0f8b2438170856d5a5289ef585515db
fasuyaaaPNG <dhavinf.a@gmail.com>
2024-01-19 13:53:09 +0700

    mobel

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,7 +16,16 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
 }
+
