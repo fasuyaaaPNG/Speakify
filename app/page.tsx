e1a7a165b00a66b7738f61ed20d43f81ff03f513
fasuyaaaPNG <dhavinf.a@gmail.com>
2024-01-24 00:59:10 +0700

    last maybe

--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,32 @@
 import './styles.css';
 import { VscArrowDown, VscDashboard, VscDebug } from 'react-icons/vsc';
 import React, { useEffect, useState } from 'react';
-import { FaLanguage, FaChartLine, FaSteam, FaTwitter,FaWrench, FaWindows, FaDownload, FaLinux, FaApple, FaAnglesUp, FaGithub, FaInstagram, FaLinkedinIn, FaYoutube, FaTiktok, FaCaretRight, FaCaretLeft} from "react-icons/fa6";
-import { motion } from 'framer-motion';
+import { FaLanguage, FaChartLine, FaSteam, FaTwitter,FaWrench, FaWindows, FaDownload, FaLinux, FaApple, FaAnglesUp, FaGithub, FaInstagram, FaTiktok, FaCaretRight, FaCaretLeft, FaCaretDown} from "react-icons/fa6";
+import { motion, useAnimation } from 'framer-motion';
 
 export default function Home() {
 
+  const [isOpen1, setIsOpen1] = useState(false);
+  const [isOpenJ1, setIsOpenJ1] = useState(false);
+  const [isOpen2, setIsOpen2] = useState(false);
+  const [isOpenJ2, setIsOpenJ2] = useState(false);
+  const [isOpen3, setIsOpen3] = useState(false);
+  const [isOpenJ3, setIsOpenJ3] = useState(false);
+
+  const handleTap1 = () => {
+    setIsOpen1(!isOpen1);
+    setIsOpenJ1(!isOpenJ1);
+  };
+
+  const handleTap2 = () => {
+    setIsOpen2(!isOpen2);
+    setIsOpenJ2(!isOpenJ2);
+  };
+
+  const handleTap3 = () => {
+    setIsOpen3(!isOpen3);
+    setIsOpenJ3(!isOpenJ3);
+  };
   const [transformX, setTransformX] = React.useState(25);
   const [count, setCount] = React.useState(0);
   
@@ -106,7 +127,7 @@ export default function Home() {
       });
     };
 
-  }, []); // Perhatikan perubahan pada dependencies array, sekarang kosong karena Anda hanya ingin ini dijalankan sekali pada mounting.
+  }, []);
 
   return (
     <div className="container">
@@ -630,9 +651,107 @@ export default function Home() {
             </div>
           </div>
         </div>
+        <motion.div
+          className="garis1"
+          initial={{ width: '100%', opacity: 1, filter: 'drop-shadow(0 0vw 0vw rgba(255, 255, 255, 0))', borderRadius: '0 0 0 0'}}
+          whileInView={{ width: '100%', opacity: 1, filter: 'drop-shadow(0 -2vw 6vw rgba(255, 255, 255, 1))', borderRadius: '0 0 15vw 15vw'}}
+          transition={{ duration: 0.5, ease: 'easeInOut' }}
+        >
+          <hr className='garis1' />
+        </motion.div>
         {/* end back 5 */}
         <div className="back6">
-          <div className="content6"></div>
+          <div className="content6">
+            <div className="judulContent6">
+              <p className="textJudulContent6">
+                FAQ
+              </p>
+            </div>
+            <div className="deskContent6">
+              <p className="textDeskContent6">
+                Things you might ask about this project
+              </p>
+            </div>
+            <div className="sectionFaq">
+              <motion.div
+                className="boxFaq"
+                onTap={handleTap1}
+                animate={{ filter: isOpen1 ? 'drop-shadow(0 0 1vw rgb(0, 225, 255))' : 'drop-shadow(0 0 0vw rgb(0, 225, 255))', scale: isOpen1 ? 0.9 : 1}}
+                transition={{ease: 'easeIn', duration: '0.2'}}
+              >
+                <p className="judulBoxFaq">
+                  What is Speakify?
+                </p>
+                  <motion.div
+                    className="iconBoxFaq"
+                    animate={{rotate: isOpen1 ? 180 : 0}}
+                  >
+                    <FaCaretDown />
+                  </motion.div>
+              </motion.div>
+              <div className={isOpenJ1 ? ' boxFaqJawaban aktif' : '`boxFaqJawaban  hilang'}>
+                <p className="jawabanBoxFaq">
+                  "Speakify" was chosen because it combines the elements "speak" which means to speak, with the suffix "-ify" which is often used to indicate transformation or change. As an app name, “Speakify” could suggest involvement in voice technology or communications enhanced using artificial intelligence.
+                </p>
+              </div>
+              <motion.div
+                className="boxFaq"
+                onTap={handleTap2}
+                animate={{ filter: isOpen2 ? 'drop-shadow(0 0 1vw rgb(0, 225, 255))' : 'drop-shadow(0 0 0vw rgb(0, 225, 255))', scale: isOpen2 ? 0.9 : 1}}
+                transition={{ease: 'easeIn', duration: '0.2'}}
+              >
+                <p className="judulBoxFaq">
+                  What was the purpose of Speakify created?
+                </p>
+                 <motion.div
+                    className="iconBoxFaq"
+                    // Properti rotasi ikon menggunakan Framer Motion
+                    animate={{rotate: isOpen2 ? 180 : 0}}
+                  >
+                    <FaCaretDown />
+                  </motion.div>
+              </motion.div>
+              <div className={isOpenJ2 ? 'boxFaqJawaban aktif' : 'boxFaqJawaban  hilang'}>
+                <p className="jawabanBoxFaq">
+                  1. Assistant for teachers and students <br /><br />
+                  Makes it easier for teachers and students to search for information on the internet using voice so they don't have to type for a long time. <br /><br />
+                  2. Access information quickly and easily <br /><br />
+                  Students and teachers can quickly access educational information via AI voice. This can make it easier to help find answers to questions. <br /><br />
+                  3. Study and improve a foreign language <br /><br />
+                  In making AI voice products, we can choose to learn 3 foreign languages ​​including English, Japanese and German. Don't worry if you have difficulty hearing foreign language answers, our product provides Indonesian translation. <br /><br />
+                  4. Increase interest in learning in the era of digitalization <br /><br />
+                  By developing our AI voice product which uses voice features so that users don't feel bored just by reading text. <br /><br />
+                  5. Utilize AI Voice Assistant technology in the education sector <br /><br />
+                  This technology also supports inclusivity by providing voice assistance to students with special needs. Thus, this technology not only increases the effectiveness of learning, but also creates a more adaptive and responsive educational environment.
+                </p>
+              </div>
+              <motion.div
+                className="boxFaq"
+                onTap={handleTap3}
+                animate={{ filter: isOpen3 ? 'drop-shadow(0 0 1vw rgb(0, 225, 255))' : 'drop-shadow(0 0 0vw rgb(0, 225, 255))', scale: isOpen3 ? 0.9 : 1}}
+                transition={{ease: 'easeIn', duration: '0.2'}}
+              >
+                <p className="judulBoxFaq">
+                  How to install Speakify?
+                </p>
+                 <motion.div
+                    className="iconBoxFaq"
+                    // Properti rotasi ikon menggunakan Framer Motion
+                    animate={{rotate: isOpen3 ? 180 : 0}}
+                  >
+                    <FaCaretDown />
+                  </motion.div>
+              </motion.div>
+              <div className={isOpenJ3 ? 'boxFaqJawaban aktif' : 'boxFaqJawaban hilang'}>
+                <p className="jawabanBoxFaq">
+                1. Press "DOWNLOAD FOR FREE" on the website <span>speakify</span><br /><br />
+                2. Choose according to the platform you use <br /><br />
+                3. Extract the downloaded file <br /><br />
+                4. Read the README.txt file because each platform has different setup methods
+                </p>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
